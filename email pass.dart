8db50d5ef69b642feb98af49b35beaa0e5478c79
Abdul Basit <134160292+AbdulBasitYounus@users.noreply.github.com>
2024-02-12 16:02:25 +0500

    Add files via upload

---
+++ email pass.dart
@@ -0,0 +1,11 @@
+
+
+void main(){
+  String email="abasit@12";
+  String pass=" sub1";
+  // String? usermail=stdin.readLineSync();
+if (email=="abasit@12" && pass=="sub12") {
+  print("login Succesfull");
+  
+}
+}
